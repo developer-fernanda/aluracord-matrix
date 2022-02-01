5949eb45271ca56524116c756fe76a81984290b7
Nanda <developer.fernanda.ingrid@gmail.com>
2022-02-01 00:00:49 -0300

    Imagens de fundo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "74ddf2a6a98e5941"; }
+/******/ 		__webpack_require__.h = function() { return "49b77c1193d6397b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
